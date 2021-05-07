9f8733613ac05ecff1e1a266e30a6b8282942004
thaisbg <thsbrgc@gmail.com>
2021-05-07 12:30:27 -0300

    Group by search

--- backend/src/main/java/com/devsuperior/dsvendas/dto/SaleDTO.java
+++ backend/src/main/java/com/devsuperior/dsvendas/dto/SaleDTO.java
@@ -4,10 +4,10 @@ import java.io.Serializable;
 import java.time.LocalDate;
 
 import com.devsuperior.dsvendas.entities.Sale;
-import com.devsuperior.dsvendas.entities.Seller;
 
 public class SaleDTO implements Serializable{
-	
+
+	private static final long serialVersionUID = 1L;
 	private Long id;
 	private Integer visited;
 	private Integer deals;
