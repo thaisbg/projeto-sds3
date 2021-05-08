692db6c57d1dd5c0cbc24a354d64dc6364770171
thaisbg <thsbrgc@gmail.com>
2021-05-08 19:26:17 -0300

    Home, navigation

--- frontend/src/pages/Dashboard/index.tsx
+++ frontend/src/pages/Dashboard/index.tsx
@@ -8,9 +8,9 @@ const Dashboard = () => {
     return (
         <>
             <NavBar />
+            
             <div className="container">
                 <h1 className="text-primary py-3">Dashboard de vendas</h1>
-
                 <div className="row px-3">
                     <div className="col-sm-6">
                         <h5 className="text-center text-secondary">Taxa de sucesso (%)</h5>
@@ -21,13 +21,12 @@ const Dashboard = () => {
                         <DonutChart />
                     </div>
                 </div>
-
                 <div className="py-3">
                     <h2 className="text-primary">Todas as vendas</h2>
                 </div>
-
                 <DataTable />
             </div>
+
             <Footer />
         </>
     );
